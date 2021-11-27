d0f3c9120f5140d673ef82a0e2e1cae31866c3a8
19UV <the19UV@gmail.com>
2021-11-27 18:55:33 -0500

    Changed Page Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'drago-digitale',
+    title: 'Drago Digitale',
     htmlAttrs: {
       lang: 'en'
     },
