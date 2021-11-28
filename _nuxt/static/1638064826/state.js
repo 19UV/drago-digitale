a0bebd3ba1caaa5ab60b273ee07adfce371def50
19UV <19UV@users.noreply.github.com>
2021-11-28 02:00:27 +0000

    deploy: 0a40d8f7fd12f969deca8de48b39647d73d24c8f

---
+++ _nuxt/static/1638064826/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdrago-digitale\u002F_nuxt\u002Fstatic\u002F1638064826",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdrago-digitale\u002F",assetsPath:"\u002Fdrago-digitale\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6f366f5b"}}}}(null));
