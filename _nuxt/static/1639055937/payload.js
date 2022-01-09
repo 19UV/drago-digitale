48acd477dbc58d4f7b91fb145f5dc60d9fd177b0
19UV <19UV@users.noreply.github.com>
2022-01-09 23:04:34 +0000

    deploy: 95d9a3ee9b250a15e70d01d57bf11c0b28c25d3c

--- _nuxt/static/1639055937/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/", {data:[{}],fetch:{"BlogCard:0":{title:"Hello World!",description:"Bought this domain, figured I'd put it to use."}},mutations:void 0});
---
+++ _nuxt/static/1641769473/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{}],fetch:{"BlogCard:0":{title:"Procedural Shakespeare",description:"Making a program write Shakespeare to annoy my English teacher."},"BlogCard:1":{title:"Hello World!",description:"Bought this domain, figured I'd put it to use."}},mutations:void 0});
