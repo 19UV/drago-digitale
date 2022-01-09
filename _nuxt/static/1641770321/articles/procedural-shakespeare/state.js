ba116b8e108db6f0411f343bcd6785dda748f5e5
19UV <19UV@users.noreply.github.com>
2022-01-09 23:38:59 +0000

    deploy: c6534b4024a66714a86cdcc015d8808a3c10c90a

--- _nuxt/static/1641770321/articles/procedural-shakespeare/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641770321",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fprocedural-shakespeare",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9f714e31"}}}}(null));
--- _nuxt/static/1641770321/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641770321",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9f714e31"}}}}(null,"\u002F"));
