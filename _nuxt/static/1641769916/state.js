d16489950e536331867d6ff62c844a1879e7b4e0
19UV <19UV@users.noreply.github.com>
2022-01-09 23:18:41 +0000

    deploy: 50786eda93d84a22f68ce054caa00123b0bab51d

--- _nuxt/static/1641769916/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641769916",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"15a7dd91"}}}}(null,"\u002F"));
